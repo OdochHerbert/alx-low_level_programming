dcb4fb50a8382c59d5e6fc1a4f3397746ba9534e
OdochHerbert <odoherb@gmail.com>
2023-02-27 22:41:05 +0300

    9-error

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,27 @@
-#include "main.h"
+/*
+ * File: 9-strcpy.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
 
 /**
- * _strcpy - Copies the string pointed to by @src, including the
- *           terminating null byte(\0) to the buffer pointed to by @dest.
- * @dest: pointer to the buffer to copy the string to.
- * @src: The string to copy
+ * _strcpy - Copies a string pointed to by @src, including the
+ *           terminating null byte, to a buffer pointed to by @dest.
+ * @dest: A buffer to copy the string to.
+ * @src: The source string to copy.
  *
  * Return: A pointer to the destination string @dest.
  */
 char *_strcpy(char *dest, const char *src)
 {
-	int i = 0;
+	int index = 0;
 
-	while (src[i])
+	while (src[index])
 	{
-		dest[i] = src[i];
-		i++;
+		dest[index] = src[index];
+		index++;
 	}
-	dest[i] = '\0';
 
 	return (dest);
 }
-
