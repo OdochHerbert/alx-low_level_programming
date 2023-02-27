3a4b20ee8b20bfda0219b5e6f412e98fdcb43b08
OdochHerbert <odoherb@gmail.com>
2023-02-27 23:23:11 +0300

    9-error8

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,13 +15,11 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
-
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-
-	return (0);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
 }
